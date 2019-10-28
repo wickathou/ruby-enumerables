bdde3d643ecd0056fc8e92ed8198f7e5575fb0b3
wickathou <javidx@gmail.com>
2019-10-28 14:32:29 -0500

    Added: + my_none? + my_count + my_map

--- module.rb
+++ module.rb
@@ -71,7 +71,42 @@ module Enumerable
     def my_any?(a=nil)
         if block_given?
             self.my_each do |x|
-               if yield(x) == nil || yield(x) == false
+               if yield(x) == true
+                return true
+               end
+            end
+            false
+        elsif a
+            if a.class == Regexp
+                self.my_each do |x|
+                    if  a.match(x.to_s)
+                        return true
+                    end
+                end
+                false
+            elsif a.class == Class
+                self.my_each do |x|
+                    if  x.is_a?(a) == true
+                        return true
+                    end
+                end
+                false
+
+            end
+        else
+            self.my_each do |x|
+               if x
+                return true
+               end
+            end
+            false
+        end
+    end
+
+    def my_none?(a=nil)
+        if block_given?
+            self.my_each do |x|
+               if yield(x) == false || yield(x) == nil
                 return true
                end
             end
@@ -103,24 +138,83 @@ module Enumerable
         end
     end
 
+    def my_count(a=nil)
+        count = 0
+        if block_given?
+            self.my_each {|x| yield(x) ? count+=1 : count}
+            return count
+        elsif a
+            self.my_each {|x| x == a ? count+=1 : count}
+            return count
+        else
+            count = self.size
+        end
+    end
+
+    def my_map
+        if block_given?
+            arr = []
+            self.my_each do |x|
+                arr.push(yield(x))
+            end
+            arr
+        else
+            self.to_enum
+        end
+    end
+
+    # def my_inject(a=nil)
+    #     final = 0
+    #     if block_given?
+    #         self.my_each {|x| final(&:+)}
+    #     else
+
+    #     end
+    # end
+
 end
 
 some = [1,2,3]
 
 
 # some.my_each{|x| puts x}
+
 # puts some.my_each
 
 # some.my_each_with_index{|x| puts x}
+
 # puts some.my_each_with_index
 
 # puts some.my_select{|a| a%2==0}
 
-puts some.my_all?(/\d/)
+# puts some.my_all?{|a| a%2==0}
 
-puts some.my_all?(String)
+# puts some.my_all?(/\d/)
 
+# puts some.my_all?(String)
 
-puts some.class
+# puts some.my_any?{|a| a%2==0}
+
+# puts some.my_any?(/\d/)
+
+# puts some.my_any?(String)
+
+# puts some.my_none?{|a| a%5==0}
+
+# puts some.my_none?(/\W/)
+
+# puts some.my_none?(String)
+
+# puts some.my_count(1)
+
+# puts some.my_count
+
+# puts some.my_count{|a| a%1==0}
+
+# puts some.my_map{|a| a*2}
+
+# puts some.my_map
+
+puts some.inject(1, :+)
 
 puts (Numeric).class
