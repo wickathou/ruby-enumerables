b21ee450bdaae9882b7f51bfa3daa5bd3811a1da
wickathou <javidx@gmail.com>
2019-11-06 13:35:41 -0500

    stickler updates

--- module.rb
+++ module.rb
@@ -23,15 +23,12 @@ module Enumerable
 
   def my_all?(a=nil)
     my_each { |x| return yield(x)? true : false } if block_given?
-    if a
-      if a.class == Regexp
-        my_each { |x| return x.to_s.match?(a)? true : false }
-      elsif a.class == Class
-        my_each { |x| return x.is_a?(a)? true : false }
-      end
-    else
-      my_each { |x| return x ? true : false }
+    if a.class == Regexp
+      my_each { |x| return x.to_s.match?(a)? true : false }
+    elsif a.class == Class
+      my_each { |x| return x.is_a?(a)? true : false }
     end
+    my_each { |x| return x ? true : false }
   end
 
   def my_any?(a=nil)
