48fd0766e053c8d842893f60e03ac5f088b0af3a
wickathou <javidx@gmail.com>
2019-11-06 13:29:02 -0500

    stickler updates

--- module.rb
+++ module.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Enumerable
   def my_each
     for i in 0...size
@@ -15,56 +16,56 @@ module Enumerable
 
   def my_select
     arr = []
-    my_each {|x| return arr.push(x) if yield(x)} if block_given?
+    my_each { |x| return arr.push(x) if yield(x)} if block_given?
     to_enum unless block_given?
   end
 
   def my_all?(a=nil)
-    my_each {|x| return yield(x)? true : false} if block_given?
+    my_each { |x| return yield(x)? true : false} if block_given?
     if a
       if a.class == Regexp
-        my_each {|x| return x.to_s.match?(a)? true : false}
+        my_each { |x| return x.to_s.match?(a)? true : false}
       elsif a.class == Class
-        my_each {|x| return x.is_a?(a)? true : false}
+        my_each { |x| return x.is_a?(a)? true : false}
       end
     else
-      my_each {|x| return x ? true : false}
+      my_each { |x| return x ? true : false}
     end
   end
 
   def my_any?(a=nil)
-    my_each {|x| return yield(x)? true : false} if block_given?
+    my_each { |x| return yield(x)? true : false} if block_given?
     if a
       if a.class == Regexp
-        my_each  {|x| return a.match(x.to_s)? true : false}
+        my_each  { |x| return a.match(x.to_s)? true : false}
       elsif a.class == Class
-        my_each  {|x| return x.is_a?(a)? true : false}
+        my_each  { |x| return x.is_a?(a)? true : false}
       end
     else
-      my_each {|x| return x ? true : false}
+      my_each { |x| return x ? true : false}
     end
   end
 
   def my_none?(a=nil)
-    my_each {|x| return yield(x) ? true : false} if block_given?
+    my_each { |x| return yield(x) ? true : false} if block_given?
     if a
       if a.class == Regexp
-        my_each  {|x| return x.to_s.match(a)? true : false}
+        my_each  { |x| return x.to_s.match(a)? true : false}
       elsif a.class == Class
-        my_each {|x| return x.is_a?(a) ? true : false}
+        my_each { |x| return x.is_a?(a) ? true : false}
       end
     else
-      my_each {|x| x ? true : false}
+      my_each { |x| x ? true : false}
     end
   end
 
   def my_count(a=nil)
     count = 0
     if block_given?
-      my_each {|x| yield(x) ? count+=1 : count}
+      my_each { |x| yield(x) ? count+=1 : count}
       return count
     elsif a
-      my_each {|x| x == a ? count+=1 : count}
+      my_each { |x| x == a ? count+=1 : count}
       return count
     else
       count = size
@@ -73,7 +74,7 @@ module Enumerable
 
   def my_map
     arr = []
-    my_each {|x| arr.push(yield(x))} if block_given?
+    my_each { |x| arr.push(yield(x))} if block_given?
     return arr if block_given?
     to_enum unless block_given?
   end
@@ -84,23 +85,23 @@ module Enumerable
     if b != nil || b != false
       case b
       when :+
-        my_each {|y| final+=y}
+        my_each { |y| final+=y}
         return final
       when :-
-        my_each {|y| final-=y}
+        my_each { |y| final-=y}
         return final
       when :*
         final = 1 if final != 0
-        my_each {|y| final*=y}
+        my_each { |y| final*=y}
         return final
       when :/
-        my_each {|y| final/=y}
+        my_each { |y| final/=y}
         return final
       end
     end
     if block_given?
       final = 1 if yield(final, self[0]) == 0 || yield(final, self[1]) == 0 if self[1] != nil
-      my_each {|x| final = yield(final,x)}
+      my_each { |x| final = yield(final,x)}
       return final
     end
   end
