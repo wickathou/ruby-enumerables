c2392a15a3397029e3c295d5e31269ecfcfb8569
wickathou <javidx@gmail.com>
2019-11-06 13:15:00 -0500

    Changes: + Code refactored and symplified + Personal method testing code separated from module

---
+++ method_testing/trials.rb
@@ -0,0 +1,105 @@
+require_relative './module.rb'
+
+
+some = [1,2,3]
+
+# Tests
+
+# multiply_els
+puts 'multiply'
+
+puts multiply_els(some)
+
+# my_each test
+
+puts 'Each'
+
+some.my_each { |x| puts x }
+
+some.my_each
+
+# my_each_with_index test
+
+puts 'Each with index'
+
+some.my_each_with_index { |x| puts x }
+
+puts some.my_each_with_index
+
+# my_select test
+
+puts 'Select'
+
+puts some.my_select { |a| a % 2 == 0 }
+
+puts some.my_select
+
+# my_all? test
+
+puts 'All'
+
+puts some.my_all? { |a| a % 2 == 0 }
+
+puts some.my_all? { |a| a % 1 == 0 }
+
+puts some.my_all?(/\D/)
+
+puts some.my_all?(/\d/)
+
+puts some.my_all?(String)
+
+# my_any? test
+
+puts 'Any'
+
+puts some.my_any? { |a| a % 2 == 0 }
+
+puts some.my_any?(/\d/)
+
+puts some.my_any?(String)
+
+# my_none? test
+
+puts 'None'
+
+puts some.my_none? { |a| a % 5 == 0 }
+
+puts some.my_none?(/\D/)
+
+puts some.my_none?(String)
+
+puts some.my_none?(Numeric)
+
+# my_count test
+
+puts 'Count'
+
+puts some.my_count(1)
+
+puts some.my_count
+
+puts (some.my_count { |a| a % 1 == 0 })
+
+# my_inject test
+
+puts 'Inject'
+
+puts (some.my_inject { |x, y| x * y })
+
+puts some.my_inject(:+)
+
+puts some.my_inject(1) { |x, y| x + y }
+
+puts some.inject{|x,y| x*y}
+
+# Map with block and with proc
+
+puts 'Map'
+
+print some.my_map { |a| a * 2 }
+
+some_proc = Proc.new { |a| a * 2 }
+
+print some.my_map(&some_proc)
+
+puts some.my_map
--- module.rb
+++ module.rb
@@ -1,121 +1,60 @@
 module Enumerable
   def my_each
-    if block_given?
-      for i in 0...size
-        yield(self[i])
-      end
-    else
-      to_enum
-    end
+    for i in 0...size
+      yield(self[i])
+    end if block_given?
+    to_enum unless block_given?
   end
 
   def my_each_with_index
-    if block_given?
-      for i in 0...size
-        yield(i)
-      end
-    else
-      to_enum
-    end
+    for i in 0...size
+      yield(i)
+    end if block_given?
+    to_enum unless block_given?
   end
 
   def my_select
     arr = []
-    if block_given?
-      my_each do |x|
-        arr.push(x) if yield(x)
-      end
-      return arr
-    else
-      to_enum
-    end
+    my_each {|x| return arr.push(x) if yield(x)} if block_given?
+    to_enum unless block_given?
   end
-  
+
   def my_all?(a=nil)
-    if block_given?
-      my_each do |x|
-        false if yield(x).nil? || yield(x) == false
-      end
-      true
-    elsif a
+    my_each {|x| return yield(x)? true : false} if block_given?
+    if a
       if a.class == Regexp
-        my_each do |x|
-          return false if a.match(x.to_s).nil? || a.match(x.to_s) == false
-        end
-        true
+        my_each {|x| return x.to_s.match?(a)? true : false}
       elsif a.class == Class
-        my_each do |x|
-          return false if x.is_a?(a).nil? || x.is_a?(a) == false
-        end
-        true
+        my_each {|x| return x.is_a?(a)? true : false}
       end
     else
-      my_each do |x|
-        false if x.nil? || x == false
-      end
-      true
+      my_each {|x| return x ? true : false}
     end
   end
 
   def my_any?(a=nil)
-    if block_given?
-      my_each do |x|
-        true if yield(x) == true
-      end
-      false
-    elsif a
+    my_each {|x| return yield(x)? true : false} if block_given?
+    if a
       if a.class == Regexp
-        my_each do |x|
-          return true if a.match(x.to_s)
-        end
-        false
+        my_each  {|x| return a.match(x.to_s)? true : false}
       elsif a.class == Class
-        my_each do |x|
-          true if x.is_a?(a) == true
-        end
-        false
-
+        my_each  {|x| return x.is_a?(a)? true : false}
       end
     else
-      my_each do |x|
-        true if x
-      end
-      false
+      my_each {|x| return x ? true : false}
     end
   end
 
   def my_none?(a=nil)
-    if block_given?
-      my_each do |x|
-         if yield(x) == false || yield(x).nil?
-        return true
-         end
-      end
-      false
-    elsif a
+    my_each {|x| return yield(x) ? true : false} if block_given?
+    if a
       if a.class == Regexp
-        my_each do |x|
-          if  a.match(x.to_s).nil? || a.match(x.to_s) == false
-            return true
-          end
-        end
-        false
+        my_each  {|x| return x.to_s.match(a)? true : false}
       elsif a.class == Class
-        my_each do |x|
-          if  x.is_a?(a).nil? || x.is_a?(a) == false
-            return true
-          end
-        end
-        false
-
+        my_each {|x| return x.is_a?(a) ? true : false}
       end
     else
-      my_each do |x|
-         if x.nil? || x == false
-        return true
-         end
-      end
-      false
+      my_each {|x| x ? true : false}
     end
   end
 
@@ -133,22 +72,15 @@ module Enumerable
   end
 
   def my_map
-    if block_given?
-      arr = []
-      my_each do |x|
-        arr.push(yield(x))
-      end
-      arr
-    else
-      to_enum
-    end
+    arr = []
+    my_each {|x| arr.push(yield(x))} if block_given?
+    return arr if block_given?
+    to_enum unless block_given?
   end
 
   def my_inject(a=nil,b=nil)
     a.class == Numeric ? final = a : final = 0
-    if a != nil && b.nil?
-      b = a
-    end
+    b = a if a != nil && b.nil?
     if b != nil || b != false
       case b
       when :+
@@ -172,83 +104,8 @@ module Enumerable
       return final
     end
   end
-
-  
-
 end
 
 def multiply_els(x)
   x.my_inject(:+)
 end
-
-some = [1,2,3]
-
-# Tests
-
-# multiply_els
-
-puts multiply_els(some)
-
-# Map with block and with proc
-
-some.my_map { |a| a * 2 }
-
-some_proc = Proc.new { |a| a * 2 }
-
-some.my_map(&some_proc)
-
-# my_each test
-
-some.my_each { |x| puts x }
-
-some.my_each
-
-# my_each_with_index test
-
-some.my_each_with_index { |x| puts x }
-
-puts some.my_each_with_index
-
-# my_select test
-
-puts some.my_select { |a| a % 2 == 0 }
-
-# my_all? test
-
-puts some.my_all? { |a| a % 2 == 0 }
-
-puts some.my_all?(/\d/)
-
-puts some.my_all?(String)
-
-# my_any? test
-
-puts some.my_any? { |a| a % 2 == 0 }
-
-puts some.my_any?(/\d/)
-
-puts some.my_any?(String)
-
-# my_none? test
-
-puts some.my_none? { |a| a % 5 == 0 }
-
-puts some.my_none?(/\W/)
-
-puts some.my_none?(String)
-
-# my_count test
-
-puts some.my_count(1)
-
-puts some.my_count
-
-puts (some.my_count { |a| a % 1 == 0 })
-
-# my_inject test
-
-puts (some.my_inject { |x, y| x * y })
-
-puts some.my_inject(:+)
-
-puts some.my_inject(1) { |x, y| x + y }
