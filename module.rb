53074e38ee0fb33390c98ebb212a18472e8f75b9
wickathou <javidx@gmail.com>
2019-11-26 12:31:19 -0500

    stickler update

--- module.rb
+++ module.rb
@@ -8,82 +8,82 @@ module Enumerable
     end
   end
 
-def my_each_with_index
-  return to_enum unless block_given?
-  for i in 0...size
-    yield(self[i], i)
+  def my_each_with_index
+    return to_enum unless block_given?
+    for i in 0...size
+      yield(self[i], i)
+    end
   end
-end
 
-def my_select
-  return to_enum unless block_given?
-  arr = []
-  my_each { |x| arr.push(x) if yield(x) }
-  arr
-end
+  def my_select
+    return to_enum unless block_given?
+    arr = []
+    my_each { |x| arr.push(x) if yield(x) }
+    arr
+  end
 
-def my_all?(a=nil)
-  self.to_a if self.is_a?(Range)
-  my_each { |x| return false if yield(x)} if block_given?
-  if a.is_a? Regexp
-    my_each { |x| return false unless a.match?(x.to_s)}
-    return true
+  def my_all?(a=nil)
+    self.to_a if self.is_a?(Range)
+    my_each { |x| return false if yield(x)} if block_given?
+    if a.is_a? Regexp
+      my_each { |x| return false unless a.match?(x.to_s)}
+      return true
+    end
+    if a.is_a? Class
+      my_each { |x| return false unless x.is_a?(a)}
+      return true
+    end
+    if a
+      my_each { |x| return false unless x==a}
+    end
+    my_each { |x| return false unless x}
+    true
   end
-  if a.is_a? Class
-    my_each { |x| return false unless x.is_a?(a)}
-    return true
-  end
-  if a
-    my_each { |x| return false unless x==a}
-  end
-  my_each { |x| return false unless x}
-  true
-end
 
-def my_any?(a=nil)
-  self.to_a if self.is_a?(Range)
-  if block_given?
-    my_each { |x| return true if yield(x)}
-    return false
+  def my_any?(a=nil)
+    self.to_a if self.is_a?(Range)
+    if block_given?
+      my_each { |x| return true if yield(x)}
+      return false
+    end
+    if a.is_a? Regexp
+      my_each { |x| return true if a.match?(x.to_s)}
+      return false
+    end
+    if a.is_a? Class
+      my_each { |x| return true if x.is_a?(a)}
+      return false
+    end
+    if a
+      my_each { |x| return true if x===a}
+      return false
+    end
+    my_each { |x| return true if x}
+    false
   end
-  if a.is_a? Regexp
-    my_each { |x| return true if a.match?(x.to_s)}
-    return false
-  end
-  if a.is_a? Class
-    my_each { |x| return true if x.is_a?(a)}
-    return false
-  end
-  if a
-    my_each { |x| return true if x===a}
-    return false
-  end
-  my_each { |x| return true if x}
-  false
-end
 
-def my_none?(a=nil,&a_block)
-  !my_any?(a,&a_block)
-end
-
-def my_count(a=nil)
-  count=0
-  return count = size unless block_given? || !a.nil?
-  my_each { |x| yield(x) ? count += 1 : count } if block_given?
-  my_each { |x| x == a ? count += 1 : count } if !a.nil? && !block_given?
-  count
-end
-
-def my_map
-  arr = []
-  to_enum unless block_given?
-  if block_given?
-    my_each { |x| arr.push(yield(x)) }
-    return arr
+  def my_none?(a=nil,&a_block)
+    !my_any?(a,&a_block)
   end
-end
 
-def my_inject(a=nil,b=nil)
+  def my_count(a=nil)
+    count=0
+    return count = size unless block_given? || !a.nil?
+    my_each { |x| yield(x) ? count += 1 : count } if block_given?
+    my_each { |x| x == a ? count += 1 : count } if !a.nil? && !block_given?
+    count
+  end
+
+  def my_map
+    arr = []
+    to_enum unless block_given?
+    if block_given?
+      my_each { |x| arr.push(yield(x)) }
+      return arr
+    end
+  end
+
+  def my_inject(a=nil,b=nil)
     arr = self.to_a.dup
     a.is_a?(Numeric) ? final = a : final = 0
     b = a if !a.nil? && b.nil?
@@ -105,11 +105,13 @@ def my_inject(a=nil,b=nil)
       end
     end
     final = 1 if yield(final, arr[0]).zero? && !arr[0].zero?
-    arr.my_each { |x| final = yield(final, x) } if block_given?
-    return final if block_given?
+    if block_given?
+      arr.my_each { |x| final = yield(final, x) }
+      return final
+    end
   end
-end
 
-def multiply_els(x)
-  x.my_inject(:*)
+  def multiply_els(x)
+    x.my_inject(:*)
+  end
 end
